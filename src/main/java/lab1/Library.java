c4ec48018f9c022d418913dcf15618c3e1fcfb12
mynameisissac <kaikaijjj123@gmail.com>
2022-09-16 01:10:46 +0800

    Initial project setup

---
+++ src/main/java/lab1/Library.java
@@ -0,0 +1,14 @@
+
+package lab1;
+
+public class Library {
+
+    public static void main(String arg[]) {
+            System.out.println("hi");
+    }
+
+    public boolean someMethod(){
+        return true;
+    }
+
+}
