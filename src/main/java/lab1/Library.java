b1d49070cd20b5c02d0f1d2f25a07f23ae855405
mynameisissac <kaikaijjj123@gmail.com>
2022-09-16 01:20:39 +0800

    Making a change

--- src/main/java/lab1/Library.java
+++ src/main/java/lab1/Library.java
@@ -4,7 +4,11 @@ package lab1;
 public class Library {
 
     public static void main(String arg[]) {
+        Library lib = new Library();
+        if (lib.someLibraryMethod()) {
             System.out.println("hi");
+        }
+
     }
 
     public boolean someMethod(){
